7e41c85a24f715e6109b94693dd703808a685866
Salem Ba-Rabuod <barboud@gmail.com>
2026-06-14 00:34:19 +0200

    project repository with all methods

---
+++ src/main/java/net/hackyourfuture/tickets/repository/ProjectRepository.java
@@ -0,0 +1,30 @@
+package net.hackyourfuture.tickets.repository;
+
+import lombok.RequiredArgsConstructor;
+import net.hackyourfuture.tickets.model.Project;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ProjectRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Project> findAllProjects() {
+        return jdbcTemplate.query(
+                "SELECT * FROM projects",
+                (rs, rowNum) -> new Project(rs.getInt("id"), rs.getString("name"))
+        );
+    }
+
+    public Project findProjectById(int id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM projects WHERE id = ?",
+                (rs, rowNum) -> new Project(rs.getInt("id"), rs.getString("name")),
+                id
+        );
+    }
+}
