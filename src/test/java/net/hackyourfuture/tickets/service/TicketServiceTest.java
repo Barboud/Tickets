5dffad5c2c25d6238fec35568e05c89ac70fe74a
Salem Ba-Rabuod <barboud@gmail.com>
2026-07-19 17:18:34 +0200

    fix: unit test TicketService

--- src/test/java/net/hackyourfuture/tickets/service/TicketServiceTest.java
+++ src/test/java/net/hackyourfuture/tickets/service/TicketServiceTest.java
@@ -60,13 +60,13 @@ class TicketServiceTest {
         Mockito.when(ticketRepository.findTicketById(1)).thenReturn(existingTicket);
 
         UpdateTicketDTO dto = new UpdateTicketDTO();
-        dto.setStatus(TicketStatus.valueOf(TicketStatus.OPEN.name()));
+        dto.setStatus(TicketStatus.valueOf(TicketStatus.CLOSED.name()));
 
         // Act
         ticketService.updateTicket(1, dto);
 
         // Assert
-        assertEquals("closed", existingTicket.getStatus());
+        assertEquals(TicketStatus.CLOSED, existingTicket.getStatus());
         assertEquals("Old Title", existingTicket.getTitle());
     }
 
